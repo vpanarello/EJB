eefb29bcc60bd7b3cdba683fc5127b1e0601498c
Vagner Panarello Filho <RM30222@fiap.com.br>
2016-08-24 21:52:40 -0300

    project

--- 03_LivrosEJB/ejbModule/br/com/fiap/bean/LivrosBean.java
+++ 03_LivrosEJB/ejbModule/br/com/fiap/bean/LivrosBean.java
@@ -3,13 +3,16 @@ package br.com.fiap.bean;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import br.com.fiap.entity.Livros;
+import br.com.fiap.interceptor.MonitorInterceptor;
 
 @Stateless
+@Interceptors({MonitorInterceptor.class})
 public class LivrosBean implements LivrosBeanRemote {
 
 	@PersistenceContext(unitName="fiapPU")
